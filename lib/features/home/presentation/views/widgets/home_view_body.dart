6339092db564d94b148840fafee2a2f05f8468e1
mohamed naif <naifm371@gmail.com>
2024-08-01 11:58:47 +0300

    create custom app bar

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'custom_app_bar.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.only(top: 18),
+      child: Column(
+        children: [
+         CustomAppBar()
+        ],
+      ),
+    );
   }
 }
