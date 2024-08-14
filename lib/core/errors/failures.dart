ce9ee787a3cf357fdf89d71923a103150848b1a7
mohamed naif <naifm371@gmail.com>
2024-08-14 17:29:27 +0300

    create server failure from dio exception

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,57 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errorMessage;
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        // Handle connection timeout
+        return ServerFailure(
+            "Connection timeout. Please check your internet connection.");
+
+      case DioExceptionType.sendTimeout:
+        // Handle send timeout
+        return ServerFailure(
+            "Send timeout. Unable to send data. Please try again later.");
+
+      case DioExceptionType.receiveTimeout:
+        // Handle receive timeout
+        return ServerFailure(
+            "Receive timeout. Unable to receive data. Please try again later.");
+
+      case DioExceptionType.badCertificate:
+        // Handle bad certificate
+        return ServerFailure(
+            "Invalid certificate. Please check your network settings.");
+
+      case DioExceptionType.badResponse:
+        // Handle bad response
+        return ServerFailure(
+            "Bad response from server. Please try again later.");
+
+      case DioExceptionType.cancel:
+        // Handle request cancellation
+        return ServerFailure("Request cancelled. Please try again.");
+
+      case DioExceptionType.connectionError:
+        // Handle connection error
+        return ServerFailure(
+            "Connection error. Please check your internet connection.");
+
+      case DioExceptionType.unknown:
+        // Handle unknown errors
+        return ServerFailure(
+            "An unknown error occurred. Please try again later.");
+
+        // defult:
+        // return ServerFailure(
+        //     "An unknown error occurred. Please try again later.");
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,16 +1,33 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 import 'package:maktabati_app/core/errors/failures.dart';
+import 'package:maktabati_app/core/utils/api_service.dart';
 
 import 'package:maktabati_app/features/home/data/models/book_model/book_model.dart';
 
 import 'home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> getBestSeellerBooks() {
-    // TODO: implement getBestSeellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> getBestSeellerBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
+
+      // List<BookModel> books = [];
+      return Right(data['items'].map((e) => BookModel.fromJson(e)).toList());
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+
+      return Left(ServerFailure(e.toString()));
+    }
   }
 
   @override
